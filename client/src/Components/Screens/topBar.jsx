bc102b662436e43724a7b5afdd504c0d44400d78
Krishnavithal <bsvskrishna.98@gmail.com>
2020-07-27 23:56:55 +0530

    Update topBar.jsx

--- client/src/Components/Screens/topBar.jsx
+++ client/src/Components/Screens/topBar.jsx
@@ -20,45 +20,45 @@ import {
 } from "@material-ui/core";
 import { UserContext } from "../../App";
 
-const useStyles1 = makeStyles((theme) => ({
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block",
-    },
-  },
-  inputRoot: {
-    color: "inherit",
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-  sectionDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex",
-    },
-  },
-  sectionMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none",
-    },
-  },
-}));
+// const useStyles1 = makeStyles((theme) => ({
+//   grow: {
+//     flexGrow: 1,
+//   },
+//   menuButton: {
+//     marginRight: theme.spacing(2),
+//   },
+//   title: {
+//     display: "none",
+//     [theme.breakpoints.up("sm")]: {
+//       display: "block",
+//     },
+//   },
+//   inputRoot: {
+//     color: "inherit",
+//   },
+//   inputInput: {
+//     padding: theme.spacing(1, 1, 1, 0),
+//     // vertical padding + font size from searchIcon
+//     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+//     transition: theme.transitions.create("width"),
+//     width: "100%",
+//     [theme.breakpoints.up("md")]: {
+//       width: "20ch",
+//     },
+//   },
+//   sectionDesktop: {
+//     display: "none",
+//     [theme.breakpoints.up("md")]: {
+//       display: "flex",
+//     },
+//   },
+//   sectionMobile: {
+//     display: "flex",
+//     [theme.breakpoints.up("md")]: {
+//       display: "none",
+//     },
+//   },
+// }));
 function TopBar(props) {
   const { state, dispatch } = useContext(UserContext);
   const history = useHistory();
